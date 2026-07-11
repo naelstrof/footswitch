4c0996cea1e216ed6985dc09310dc775a7312a71
naelstrof <naelstrof@gmail.com>
2026-07-11 15:14:19 -0600

    updated homebrew formula to run cmake, and to pass brew audit

--- HomebrewFormula/footswitch.rb
+++ HomebrewFormula/footswitch.rb
@@ -4,11 +4,13 @@ class Footswitch < Formula
   license "MIT"
   head "https://github.com/rgerganov/footswitch.git"
 
+  depends_on "cmake" => :build
   depends_on "hidapi"
   depends_on "pkg-config"
 
   def install
-    system "mkdir", "#{prefix}/bin"
+    mkdir bin
+    system "cmake", "./", "-DCMAKE_INSTALL_PREFIX=#{prefix}"
     system "make", "install", "PREFIX=#{prefix}"
   end
 end
